6fc7fe929368b81fdda0af3935ab22646f909437
Bryan Wills <bryanwi09@gmail.com>
2025-07-08 13:45:57 -0400

    Remove tokyonight theme and configuration, switch to onemonokai as the only theme. Clean up plugin loading to ensure only onemonokai is used.

--- lua/bryan/plugins/init.lua
+++ lua/bryan/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  require("bryan.plugins.theme"),
 }
 --require("bryan.plugins.theme")
